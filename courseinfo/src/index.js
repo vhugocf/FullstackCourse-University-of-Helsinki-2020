84ce0750653a138d89cb6823fb3888d18969cdbd
Victor Hugo C Flores <vhugocf@hotmail.com>
2019-11-06 16:44:04 -0300

    in progress

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const App = () => {
+
+    const course = {
+        
+        name: "Half Stack application development",
+
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1
+            }
+        ]// fim parts
+    }// fim const
+
+    return (
+        <>
+
+        <h1> teste </h1>
+
+
+        </>
+    )// fim retunr
+
+}// fim app
+
+
+const Course = (props) => {
+
+
+
+
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
