aa306599438bf059705ab039fa90aea46fd32f60
Victor Hugo C Flores <vhugocf@hotmail.com>
2020-01-09 12:08:29 -0300

    correction

---
+++ courseinfo/src/components/Content.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Part from './Part';
+
+
+const Content = ( {course} ) => {
+
+    return (
+        <>
+
+        {course.parts.map((part) => 
+        
+        <Part key={part.name} part={part.name} exercises={part.exercises}/>  )}
+        
+        
+
+        </>
+
+    )
+
+}
+
+
+export default Content;
+
+
+
